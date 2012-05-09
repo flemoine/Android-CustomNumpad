c3e1d774b43f3004f57b1361bf4934c782fffa87
gaara <gaara@gaara-laptop.(none)>
2012-05-09 14:02:27 +0530

    Finished

--- src/akash/app/customnumpad/CustomNumpadActivity.java
+++ src/akash/app/customnumpad/CustomNumpadActivity.java
@@ -1,6 +1,22 @@
+/*     
+     Copyright (C) 2012 - Akash Ramani
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
 package akash.app.customnumpad;
 
 import android.app.Activity;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 
 public class CustomNumpadActivity extends Activity {
@@ -9,5 +25,13 @@ public class CustomNumpadActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Get Numpad to set current activity to dispatch key events
+        CustomNumpadView cnv = (CustomNumpadView) findViewById(R.id.numpadView);
+        cnv.setActionListenerActivity(this);
+        
+        //create new android keyboard object to set it as default keyboard for current view
+        Keyboard keyboard = new Keyboard(this, R.xml.keyboard);
+        cnv.setKeyboard(keyboard);
     }
 }
--- src/akash/app/customnumpad/CustomNumpadView.java
+++ src/akash/app/customnumpad/CustomNumpadView.java
@@ -1,3 +1,18 @@
+/*     
+     Copyright (C) 2012 - Akash Ramani
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
 package akash.app.customnumpad;
 
 import android.app.Activity;
@@ -8,15 +23,28 @@ import android.view.KeyEvent;
 
 public class CustomNumpadView extends KeyboardView {
 
-    static CustomOnKeyboardActionListener keyListener;
+    CustomOnKeyboardActionListener keyListener;
     
     public CustomNumpadView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        keyListener = new CustomOnKeyboardActionListener();
     }
+    
+    public void setActionListenerActivity(Activity act){
+        keyListener = new CustomOnKeyboardActionListener(act);
+        this.setOnKeyboardActionListener(keyListener);
+    }
+    
+    @Override
+    public boolean isInEditMode() {
+        return true;
+    };
     
     private class CustomOnKeyboardActionListener implements OnKeyboardActionListener {
         Activity owner;
+        
+        public CustomOnKeyboardActionListener(Activity activity){
+            owner = activity;
+        }
         
         @Override
         public void onKey(int primaryCode, int[] keyCodes) {
